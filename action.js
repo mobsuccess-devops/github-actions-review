571d18811ce7419ea305dc2cb49797440875ccd9
Philippe Auriach <p.auriach@gmail.com>
2023-03-01 09:38:42 +0100

    Add merge group support (#42)

--- action.js
+++ action.js
@@ -22,14 +22,21 @@ async function getActionParameters() {
     } = github.context.payload;
     console.log(`Found issue ${issueNumber} for ${owner}/${repo}`);
     return { comment, owner, repo, issueNumber };
-  } else {
+  } else if (github.context.payload.pull_request) {
     // this is a PR event
     console.log("Getting PR from context");
     const pullRequest = github.context.payload.pull_request;
     return {
       pullRequest,
     };
+  } else if (github.context.payload.merge_group) {
+    // this is a merge group event
+    const { merge_group: mergeGroup } = github.context.payload;
+    return {
+      mergeGroup,
+    };
   }
+  return {};
 }
 
 const getPullAuthor = (exports.getPullAuthor = (pullRequest) => {
@@ -84,8 +91,10 @@ exports.action = async function action() {
     github.context.payload.issue.number
   ) {
     return await actionIssue();
-  } else {
+  } else if (github.context.payload.pull_request) {
     return await actionPullRequest();
+  } else {
+    console.log("Not an issue neither a pull request, ignoring");
   }
 };
 
